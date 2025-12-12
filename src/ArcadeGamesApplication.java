e1a60061f88978f16a91da7d93b69aa80c7a23f8
Tiago Rustioni <t.rustion@itsprodigi.it>
2025-12-12 15:18:29 +0100

    Add choice 2 and 3 (add break end error comand)

--- src/ArcadeGamesApplication.java
+++ src/ArcadeGamesApplication.java
@@ -1,18 +1,62 @@
 import java.util.Scanner;
 
+import service.ArcadeService;
+
 public class ArcadeGamesApplication {
 
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
 		
+		boolean isRunning = true;
 		
-		System.out.println("Scegli comando ");
-		System.out.println("1) Aggiungi gioco alla lista: ");
-		System.out.println("2) Cerca il gioco che vuoi: ");
-		System.out.println("3) Gioci Difficile (Livello di difficoltà > 4)");
+		ArcadeService arcadeService = new ArcadeService();
 		
+		while (isRunning)
+		{
+			System.out.println("Scegli comando ");
+			System.out.println("1) Aggiungi gioco alla lista: ");
+			System.out.println("2) Vedi tutta la lista ");
+			System.out.println("3) Cerca il gioco che vuoi: ");
+			System.out.println("4) Gioci Difficile (Livello di difficoltà > 4)");
+			System.out.println("5) Esci ");
+			
+			String choose = scanner.nextLine();
+			
+			if (choose.equals("1"))
+			{
+				System.out.println("Inserisci il nome del nuovo gioco: ");
+				String name = scanner.nextLine();
+				System.out.println("Inserisci la data di uscita del nuovo gioco: ");
+				int release = scanner.nextInt();
+				System.out.println("Inserisci la difficoltà del nuovo gioco: ");
+				int difficultLevel = scanner.nextInt();
+				
+				arcadeService.insertGame(name, release, difficultLevel);
+			}
+			
+			else if (choose.equals("2"))
+			{
+				arcadeService.printGame();
+			}
+			
+			else if (choose.equals("3"))
+			{
+				System.out.println("Quale gioco vuoi cercare? ");
+				String gameName = scanner.nextLine();
+				arcadeService.searchGame(gameName);
+			}
+			
+			else if (choose.equals("5"))
+			{
+				isRunning = false;
+				System.out.println("Ciao!1 <3");
+			}
+			
+			
+		}
 		
+		scanner.close();
 
 	}
 
--- src/service/ArcadeService.java
+++ src/service/ArcadeService.java
@@ -2,12 +2,35 @@ package service;
 
 import java.util.ArrayList;
 
+import models.Arcade;
+
 public class ArcadeService {
 	
-	private ArrayLIst<Arcade> arcade = new ArrayList<Arcade>();
+	private ArrayList<Arcade> arcades = new ArrayList<Arcade>();
 
+	public void insertGame(String n, int r, int d)
+	{
+		Arcade arcade = new Arcade(n,r,d);
+		this.arcades.add(arcade);
+	}
 	
+	public void printGame()
+	{
+		for (int i = 0; i < this.arcades.size() ; i++)
+		{
+			System.out.println(this.arcades.get(i));
+		}
+	}
 	
-	
+	public void searchGame(String search)
+	{
+		for (int i = 0; i < this.arcades.size(); i++)
+		{
+			if (this.arcades.get(i).getName().equals(search));
+			{
+				System.out.println("Il nome del gioco è " + this.arcades.get(i).getName()+" è uscito l'anno "+this.arcades.get(i).getRelease()+" e la sua difficoltà è "+this.arcades.get(i).getDifficultLevel());
+			}
+		}
+	}
 
 }
